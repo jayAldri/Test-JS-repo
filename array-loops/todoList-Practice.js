4da28f99242ec66a726f1cdc9f5cd80655b54cc6
Jay Aldrin <jayaldrinorabi015@gmail.com>
2025-09-15 12:57:42 +0800

    array and loops

--- array-loops/todoList-Practice.js
+++ array-loops/todoList-Practice.js
@@ -1,8 +1,34 @@
-const todoList = [];
+const todoList = ['make coffee', 'Drink Coffee'];
+
+renderTodoList();
+
+function renderTodoList () {
+
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+
+  }
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list').innerHTML =  todoListHTML;
+}
+
+
 
 function addToDo() {
   const inputElem = document.querySelector('.js-name-input');
 
   const name = inputElem.value;
   console.log(name);
+
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElem.value = '';
+
+  renderTodoList();
 }
