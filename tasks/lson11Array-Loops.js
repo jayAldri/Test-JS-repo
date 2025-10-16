b824a7e1d5387874cefc425364a88acfa95a3b67
Jay Aldrin O. Rabi <jayaldrinorabi015@gmail.com>
2025-10-16 10:22:17 +0800

    add exercise 11i and 11j

--- tasks/lson11Array-Loops.js
+++ tasks/lson11Array-Loops.js
@@ -23,3 +23,26 @@ console.log(arraySwap(['hi', 'babi', 'have a nice day'])); // ['have a nice day'
 for(let i = 0; i <= 10; i += 2){
   console.log(i);
 }
+
+// 11i. Create a function addNum(array, num) that takes an array of number and returns an array where each nimber is increased by 'num'
+function addNum(array, num){
+  const newArray = [];
+  for(let i = 0; i < array.length; i++){
+    newArray.push(array[i] + num);
+  }
+  return newArray;
+}
+console.log(addNum([1,2,3], 2)); // [3,4,5]
+console.log(addNum([1,2,3], 3)); // [4,5,6]
+console.log(addNum([-1,-2,0, 99], 2)); // [1,0,2,101]
+
+function addArray(array1, array2){
+  const newArray = [];
+
+  for(let i = 0; i < array1.length; i++){
+    newArray.push(array1[i] + array2[i]);
+  }
+  return newArray;
+}
+console.log(addArray([1,1,2], [1,1,3])); // [2,2,5]
+console.log(addArray([1,2,3], [4,5,6])); // [5,7,9]
