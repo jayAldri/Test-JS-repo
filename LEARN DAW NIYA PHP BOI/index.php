97313715f19b2637aa2614590dc29b962d2bff75
Jay Aldrin O. Rabi <jayaldrinorabi015@gmail.com>
2026-01-05 14:12:15 +0800

    learn niya daw yung php nayan chat

---
+++ LEARN DAW NIYA PHP BOI/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Learn PHP 
+
+  </title>
+</head>
+<body>
+
+<?php
+  $s_Address1 = "123 Main St";
+  $s_Address2 = "Apt 4B";
+  $s_City = "Springfield";
+  echo "<h1> Address Line 1: </h1>" . $s_Address1 . "<br>";
+  echo "<h1> Address Line 2: </h1>" . $s_Address2 . "<br>";
+  echo "<h1> City: </h1>" . $s_City . "<br>";
+?>
+  
+</body>
+</html>
